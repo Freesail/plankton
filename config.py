9d950098eca740582de4a41ea6b243311f39bade
Freesail <imperialfreesail@gmail.com>
2020-03-11 22:50:33 +0000

    dropout for fc

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ data_transforms = dict()
 data_transforms['train'] = transforms.Compose([
     preprocess.PadToSquare(),
     transforms.RandomAffine(degrees=180, translate=(0.1, 0.1),
-                            scale=(0.7, 1.2), shear=30, fillcolor=(255, 255, 255)),
-    transforms.ColorJitter(contrast=[0.7, 1.1]),
+                            scale=(0.7, 1.2), shear=20, fillcolor=(255, 255, 255)),
+    transforms.ColorJitter(contrast=[0.7, 1.2]),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomVerticalFlip(p=0.5),
     transforms.Resize((224, 224)),
@@ -31,9 +31,9 @@ train_cfg = {
 }
 
 model_cfg = {
-    'backbone': 'wide_resnet50_2',
+    'backbone': 'resnet18',
     'pretrained': True,
-    'fc_hidden_dim': [1024, 1024],
+    'fc_hidden_dim': [512, 512],
     'tune_conv': True,
     'fc_dropout': 0.5,
     'num_classes': 121,
@@ -42,7 +42,7 @@ model_cfg = {
 
 optimizer_cfg = {
     'lr': 5e-4,
-    'conv_lr_ratio': 0.3
+    'conv_lr_ratio': 0.4
 }
 
 scheduler_cfg = {
