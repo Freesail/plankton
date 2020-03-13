52789c1f6277e17771efdf79d8c53e521ffbde7a
Sibo Zhao <36244726+Siboooo@users.noreply.github.com>
2020-03-13 15:50:56 +0000

    1 layer fc

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ train_cfg = {
 model_cfg = {
     'backbone': 'resnet18',
     'pretrained': True,
-    'fc_hidden_dim': [512, 512],
+    'fc_hidden_dim': [],
     'tune_conv': True,
     'fc_dropout': 0.5,
     'num_classes': 121,
@@ -42,7 +42,7 @@ model_cfg = {
 
 optimizer_cfg = {
     'lr': 5e-4,
-    'conv_lr_ratio': 0.4
+    'conv_lr_ratio': 0.2
 }
 
 scheduler_cfg = {
